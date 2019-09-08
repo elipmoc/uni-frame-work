edee2caed1b9f96e8283e4a2ac991ce36cd8fc82
elipmoc <relipmoc101@gmail.com>
2019-09-08 19:23:06 +0900

    templateフォルダ作成

---
+++ Template/Scripts/BigBang/Scene/SampleSceneCreatorCreator.cs
@@ -0,0 +1,15 @@
+﻿using Domain;
+using GameSystem.Scene;
+using UniFrameWork.Domain.Core;
+using UniFrameWork.Domain.Core.Scene;
+
+namespace BigBang.Scene
+{
+    public class SampleSceneCreatorCreator : ISceneCreatorCreator<SceneChanger>
+    {
+        public SceneCreator Create(SceneChanger sceneChanger)
+        {
+            return () => new SampleScene(sceneChanger);
+        }
+    }
+}
---
+++ Template/Scripts/God/God.cs
@@ -0,0 +1,28 @@
+﻿using BigBang.Scene;
+using UniFrameWork.BigBang.Core;
+using UniFrameWork.GameSystem.Core.Scene;
+using UnityEngine;
+using SceneChanger = Domain.SceneChanger;
+
+namespace God
+{
+    public class God : MonoBehaviour
+    {
+        SceneManager sceneManager;
+
+        void Start()
+        {
+            var firstSceneCreatorCreator = new SampleSceneCreatorCreator();
+            var sceneChanger = new SceneChanger(firstSceneCreatorCreator);
+            sceneManager = new SceneManagerCreator(
+                firstSceneCreatorCreator.Create(sceneChanger)
+            ).Create();
+            sceneChanger.ChangedScene.Subscribe(sceneCreator => sceneManager.SetScene(sceneCreator));
+        }
+
+        void Update()
+        {
+            sceneManager.Update();
+        }
+    }
+}
